be205640a816b3729cf7b5702496f34730aff056
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-10-01 22:42:50 +0530

    Create insert_at_beginning.c

---
+++ insert_at_beginning.c
@@ -0,0 +1,11 @@
+void insert_beg(int ele)
+{
+    struct node *tmp = (struct node*)malloc(sizeof(struct node));
+    tmp->data = ele;
+    if(p == NULL) {
+        tmp->next = NULL;
+    } else {
+        tmp->next = p;
+    }
+    p = tmp;
+}
