5577426cd0eb9dc0a6e03b539a4db5980845c6da
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-10-02 02:15:35 +0530

    Update insert_bt.c

--- insert_bt.c
+++ insert_bt.c
@@ -9,3 +9,11 @@ struct bt * insert(struct bt *q,int val, char dir)
     else  
         q->right=tmp;  
 }
+
+// In main ()  
+struct bt *root=(struct bt *)malloc(sizeof(struct bt));  
+root->data=-1;  
+insert(root,3,'l');  
+insert(root,2,'r');  
+insert(root->left,5,'l');  
+insert(root->right,7,'l');
